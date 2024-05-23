118d602976f8ca8637848b4ba911fa7fbeb12604
Maksym Soloviov <42580651+soloveyko23@users.noreply.github.com>
2024-05-23 15:19:06 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+(() => {
+    "use strict";
+    function addLoadedClass() {
+        if (!document.documentElement.classList.contains("loading")) window.addEventListener("load", (function() {
+            setTimeout((function() {
+                document.documentElement.classList.add("loaded");
+            }), 0);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    window["FLS"] = true;
+    addLoadedClass();
+})();
